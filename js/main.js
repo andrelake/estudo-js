8af398ce0b35494c30bda5d0e8381a46825e55e8
andrelake <andre.lake@hotmail.com>
2020-06-23 14:45:22 -0300

    Var

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert("teste de alert");
+var nome = "Isaac Newton"
+
+//alert("teste de alert");
+//alert("Bem vindo, " + nome);
+
+console.log(nome);
+console.log(nome.replace("Isaac Newton", "Carl Sagan"))
+console.log(nome.toUpperCase());
+console.log(nome.toLowerCase());
