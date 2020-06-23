25dccee14dc4d3965bea5958e2143caefb816f73
andrelake <andre.lake@hotmail.com>
2020-06-23 14:54:06 -0300

    Array

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
-var nome = "Isaac Newton"
+//var nome = "Isaac Newton"
 
 //alert("teste de alert");
 //alert("Bem vindo, " + nome);
 
-console.log(nome);
-console.log(nome.replace("Isaac Newton", "Carl Sagan"))
-console.log(nome.toUpperCase());
-console.log(nome.toLowerCase());
+//console.log(nome);
+//console.log(nome.replace("Isaac Newton", "Carl Sagan"))
+//console.log(nome.toUpperCase());
+//console.log(nome.toLowerCase());
+//-----------------------------------------------------------------
+/*
+var listaTeste = ["aaa", "bbb", "ccc", "ddd"]
+listaTeste.push("eee")
+listaTeste.pop()
+
+console.log(listaTeste)
+console.log(listaTeste.toString())
+*/
+
+var carro = {nome: "Fusca", cor: "Azul"}
+console.log(carro)
+
+var carros = [{nome: "Fusca", cor: "Azul"}, {nome: "Gol", cor: "Branco"}]
+console.log(carros)
