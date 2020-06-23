69109a53d3864da88ac0ec771e57b3180a0788f4
andrelake <andre.lake@hotmail.com>
2020-06-23 15:08:17 -0300

    While, for, date

--- js/main.js
+++ js/main.js
@@ -16,9 +16,37 @@ listaTeste.pop()
 console.log(listaTeste)
 console.log(listaTeste.toString())
 */
-
+/*
 var carro = {nome: "Fusca", cor: "Azul"}
 console.log(carro)
 
 var carros = [{nome: "Fusca", cor: "Azul"}, {nome: "Gol", cor: "Branco"}]
 console.log(carros)
+*/
+//-----------------------------------------------------------------
+/*
+var idade = prompt("Digite sua idade")
+
+if(idade >= 18) {
+    alert("Maior de idade")
+} else{
+    alert("Menor de idade")
+}
+*/
+/*
+var i = 0;
+while (i < 10) {
+    console.log(i)
+    i++
+}
+*/
+/*
+for(var i=0; i<5; i++) {
+    console.log(i)
+}
+*/
+//-----------------------------------------------------------------
+var date = new Date()
+
+console.log(date.getDay() + "/" + (date.getMonth()+1) + "/" + date.getFullYear())
+console.log(date)
